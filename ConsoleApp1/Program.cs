3280538cb810aed811856c2dabcf1a27e6b0cea1
raiyuvraj <yuvirai017@gmail.com>
2022-12-20 17:51:30 -0800

    [Yuvraj]-Last Name

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Last Name: ");
+        var data = Console.ReadLine();
+        string pattern = "^[A-Z]{1}[a-z]{2,}$";
+
+        Regex regex = new Regex(pattern);
+
+        if (regex.IsMatch(data))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("It is Valid name");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid");
+            Console.ResetColor();
+        }
+        
+    }
+}
